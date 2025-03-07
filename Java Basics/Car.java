54a526c837556c8b01866714d204e65fe093d76f
Kavitha-Sri14 <kavithasrisaikia@gmail.com>
2025-03-07 18:36:02 +0530

    Add files via upload

---
+++ Java Basics/Car.java
@@ -0,0 +1,31 @@
+
+public class Car {
+    
+   
+    String make;
+    String model;
+    int year;
+
+    
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    
+    public void displayCarInfo() {
+        System.out.println("Car Make: " + make);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Year: " + year);
+    }
+
+    
+    public static void main(String[] args) {
+        
+        Car myCar = new Car("Toyota", "Corolla", 2025);
+        
+        
+        myCar.displayCarInfo();
+    }
+}
---
+++ Java Basics/CommentsExample.java
@@ -0,0 +1,26 @@
+public class CommentsExample {
+
+   
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello, World!");
+
+        
+        System.out.println("Welcome to Java Programming!");
+
+        
+        int result = square(5);  
+        System.out.println("Square of 5 is: " + result);
+    }
+
+    /**
+     * This method calculates the square of an integer.
+     * @param num The number to be squared.
+     * @return The square of the number.
+     */
+    public static int square(int num) {
+        return num * num;
+    }
+}
+
